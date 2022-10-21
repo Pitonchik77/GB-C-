8cf737315d9eb16223e532fb7c475f9f0642d223
pitonchik77 <petr_77_@mail.ru>
2022-10-21 21:35:53 +0700

    Первая задача на C#

---
+++ Task0/Program.cs
@@ -0,0 +1,4 @@
+﻿// Написать программу, которая на вход принимает число и выдаёт его квадрат
+Console.Write("Введите число: ");
+int InputNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Квадрат числа " + InputNumber + " равен " + InputNumber * InputNumber);
